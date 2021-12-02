4fc9cba15faa24ad076556c34be8abf9c2c540f9
James Coleman <mrjameslcoleman@gmail.com>
2021-12-02 21:18:45 +0000

    Fixed bug but answer too high

--- Advent of Code 2015/Day 9.playground/Contents.swift
+++ Advent of Code 2015/Day 9.playground/Contents.swift
@@ -127,7 +127,7 @@ let exampleInput = """
     Dublin to Belfast = 141
     """
 
-var cities = [City]()
+var exampleCities = [City]()
 
 for line in exampleInput.components(separatedBy: .newlines) {
     let components = line.components(separatedBy: .whitespaces)
@@ -139,25 +139,97 @@ for line in exampleInput.components(separatedBy: .newlines) {
     let city1 = City(name: city1Name)
     let city2 = City(name: city2Name)
     
-    if cities.contains(city1) == false {
-        cities.append(city1)
+    if exampleCities.contains(city1) == false {
+        exampleCities.append(city1)
     }
     
-    if cities.contains(city2) == false {
-        cities.append(city2)
+    if exampleCities.contains(city2) == false {
+        exampleCities.append(city2)
     }
     
-    cities.first(where: { $0 == city1 })?[city2] = distance
+    let realCity1 = exampleCities.first(where: { $0 == city1 })!
+    let realCity2 = exampleCities.first(where: { $0 == city2 })!
+    
+    realCity1[realCity2] = distance
 }
 
-cities.count
+exampleCities.count
 
 var exampleRoute = Route()
 
-for city in cities {
+for city in exampleCities {
     exampleRoute.append(city: city)
 }
 
 exampleRoute.cities
 exampleRoute.visualRoute
-exampleRoute.shortestDistance
+exampleRoute.shortestDistance // 605 (correct)
+
+let challengeInput = """
+    Faerun to Tristram = 65
+    Faerun to Tambi = 129
+    Faerun to Norrath = 144
+    Faerun to Snowdin = 71
+    Faerun to Straylight = 137
+    Faerun to AlphaCentauri = 3
+    Faerun to Arbre = 149
+    Tristram to Tambi = 63
+    Tristram to Norrath = 4
+    Tristram to Snowdin = 105
+    Tristram to Straylight = 125
+    Tristram to AlphaCentauri = 55
+    Tristram to Arbre = 14
+    Tambi to Norrath = 68
+    Tambi to Snowdin = 52
+    Tambi to Straylight = 65
+    Tambi to AlphaCentauri = 22
+    Tambi to Arbre = 143
+    Norrath to Snowdin = 8
+    Norrath to Straylight = 23
+    Norrath to AlphaCentauri = 136
+    Norrath to Arbre = 115
+    Snowdin to Straylight = 101
+    Snowdin to AlphaCentauri = 84
+    Snowdin to Arbre = 96
+    Straylight to AlphaCentauri = 107
+    Straylight to Arbre = 14
+    AlphaCentauri to Arbre = 46
+    """
+
+var challengeCities = [City]()
+
+for line in challengeInput.components(separatedBy: .newlines) {
+    let components = line.components(separatedBy: .whitespaces)
+    
+    let city1Name = components[0]
+    let city2Name = components[2]
+    let distance = Int(components[4])!
+    
+    let city1 = City(name: city1Name)
+    let city2 = City(name: city2Name)
+    
+    if challengeCities.contains(city1) == false {
+        challengeCities.append(city1)
+    }
+    
+    if challengeCities.contains(city2) == false {
+        challengeCities.append(city2)
+    }
+    
+    let realCity1 = challengeCities.first(where: { $0 == city1 })!
+    let realCity2 = challengeCities.first(where: { $0 == city2 })!
+    
+    realCity1[realCity2] = distance
+}
+
+challengeCities.count
+
+var challengeRoute = Route()
+
+for city in challengeCities {
+    challengeRoute.append(city: city)
+}
+
+challengeRoute.cities
+challengeRoute.visualRoute // Straylight -> Tambi -> Snowdin -> Norrath -> Tristram -> Arbre -> AlphaCentauri -> Faerun = 192 (too high)
+challengeRoute.shortestDistance // 192 (too high)
